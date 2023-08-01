dc3578648d635a7f6dbfe07abdeaeabd4cca9830
Sreesanth46 <sreesanth46@gmail.com>
2023-08-01 17:43:10 +0530

    chore: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve, dirname } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+        entry: resolve(dirname(''), 'src/main.ts'),
+        name: 'VueTable',
+        fileName: (format) => `${format}.js`
+    },
+    rollupOptions: {
+        external: ['vue'],
+        output: {
+            globals: {
+                vue: 'Vue'
+            }
+        }
+    }
+}
 })
