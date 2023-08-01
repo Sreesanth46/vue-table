ce39c50a6192233bfbaf7c87e0a4b2e0100c82ab
Sreesanth46 <sreesanth46@gmail.com>
2023-08-01 18:15:03 +0530

    chore: example table

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,10 +1,17 @@
+<script setup>
+import { VueTable } from "../../dist/es.js";
+
+const headers = ["name", "age"];
+const keyValues = ["name", "age"];
+const datas = [
+    { name: "example 1", age: 19 },
+    { name: "example 2", age: 22 },
+    { name: "example 3", age: 40 },
+];
+</script>
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+    <div>
+        <VueTable :headers="headers" :keys="keyValues" :data="datas" />
+    </div>
 </template>
