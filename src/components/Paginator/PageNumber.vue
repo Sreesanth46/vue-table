84b4f69aa0a9f3a142f6a1e8a89c0b9bebb21412
Sreesanth <sreesanth46@gmail.com>
2023-11-05 02:07:51 +0530

    feat: active class, refactor: removed dark prop

--- src/components/Paginator/PageNumber.vue
+++ src/components/Paginator/PageNumber.vue
@@ -1,37 +1,26 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-
-const props = withDefaults(
+withDefaults(
   defineProps<{
     large: boolean;
-    dark: boolean;
     active: boolean;
   }>(),
   {
     large: false,
-    dark: false,
     active: false
   }
 );
-
-const classObject = computed(() => ({
-  'z-10 text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700':
-    !props.dark && props.active,
-  'z-10 dark:border-gray-700 dark:bg-gray-700 dark:text-white':
-    props.dark && props.active,
-  'dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white':
-    props.dark && !props.active
-}));
 </script>
 
 <template>
   <li>
     <button
       :aria-current="active ? 'page' : undefined"
-      class="flex items-center justify-center leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+      class="flex items-center justify-center leading-tight"
       :class="[
         large ? 'px-4 h-10' : 'px-3 h-8',
-        classObject
+        active
+          ? 'z-10 text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'
+          : 'text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
       ]">
       <slot></slot>
     </button>
