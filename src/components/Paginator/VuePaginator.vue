d8dc94797158bf238a08f440f9d4cb99ab3275da
Sreesanth <sreesanth46@gmail.com>
2023-11-04 16:56:42 +0530

    feat: pagination

---
+++ src/components/Paginator/VuePaginator.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    large: boolean;
+    dark: boolean;
+    active: boolean;
+  }>(),
+  {
+    large: false,
+    dark: false,
+    active: false
+  }
+);
+
+import PreviousNavigation from './PreviousNavigation.vue';
+import NextNavigation from './NextNavigation.vue';
+import PageNumber from './PageNumber.vue';
+</script>
+
+<template>
+  <nav aria-label="Page navigation example">
+    <ul class="flex items-center -space-x-px h-8 text-sm">
+      <PreviousNavigation :dark="dark" :large="large" />
+      <page-number
+        :dark="dark"
+        :large="large"
+        v-for="item in [1, 2, 3, 4, 5]"
+        :key="item"
+        :active="item === 3"
+        >{{ item }}</page-number
+      >
+      <NextNavigation :dark="dark" :large="large" />
+    </ul>
+  </nav>
+  <br />
+  <nav aria-label="Page navigation example">
+    <ul class="flex items-center -space-x-px h-10 text-base">
+      <PreviousNavigation :dark="dark" :large="!large" />
+      <page-number :dark="dark" :large="!large" :active="active" v-for="item in [1, 2, 3, 4, 5]" :key="item">{{
+        item
+      }}</page-number>
+      <NextNavigation :dark="dark" :large="!large" />
+    </ul>
+  </nav>
+</template>
