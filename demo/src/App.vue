b95a4802914465f4c9e48d97c3e439dc5951f543
Sreesanth <sreesanth46@gmail.com>
2023-11-05 18:44:27 +0530

    feat: example with pagination

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,32 +1,68 @@
 <script setup>
-import { VueTable, VuePaginator } from "../../dist/es.js";
-import "../../dist/style.css";
+import { VueTable, VuePaginator } from '../../dist/es.js';
+import '../../dist/style.css';
+import data from './assets/data.json';
+import { computed, ref, watch } from 'vue';
 
-const headers = ["name", "age"];
-const keyValues = ["name", "age"];
-const data = [
-    { name: "example 1", age: 19 },
-    { name: "example 2", age: 22 },
-    { name: "example 3", age: 40 },
+const headers = [
+  'id',
+  'name',
+  'DOB',
+  'GPA',
+  'course',
+  'department',
+  'fees paid'
 ];
+const keyValues = [
+  'id',
+  'name',
+  'date_of_birth',
+  'GPA',
+  'course',
+  'department',
+  'fees_paid'
+];
+
+const itemsPerPage = 8;
+
+const startOffSet = ref(0);
+const endOffSet = ref(startOffSet.value + itemsPerPage);
+
+watch(startOffSet, nOff => {
+  endOffSet.value = startOffSet.value + itemsPerPage;
+});
+
+const pageCount = Math.ceil(data.length / itemsPerPage);
+
+const currentData = computed(() =>
+  data.slice(startOffSet.value, endOffSet.value)
+);
+
+function onPageChange(pageNumber) {
+  const newOffSet = (pageNumber - 1) * itemsPerPage;
+  startOffSet.value = newOffSet;
+}
 </script>
 
 <template>
+  <div
+    style="
+      width: 90%;
+      position: absolute;
+      left: 0;
+      right: 0;
+      margin: 0 auto;
+      margin-top: 6%;
+    ">
+    <VueTable :headers="headers" :keys="keyValues" :data="currentData" />
     <div
-        style="
-            width: 90%;
-            position: absolute;
-            left: 0;
-            right: 0;
-            margin: 0 auto;
-            margin-top: 6%;
-        "
-    >
-        <VueTable :headers="headers" :keys="keyValues" :data="data" />
-        <div style="display: flex; flex-direction: column; align-items: center; margin-top: 2rem;">
-            <VuePaginator />
-            <br>
-            <VuePaginator large/>
-        </div>
+      style="
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 2rem;
+      ">
+      <VuePaginator :pageCount="pageCount" @onPageChange="onPageChange" />
     </div>
+  </div>
 </template>
