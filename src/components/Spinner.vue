a5c06634b0fb364ee1ea42c2271d0b2860bb492e
Sreesanth <sreesanth46@gmail.com>
2024-01-06 12:28:28 +0530

    chore: loading spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="inline-block h-8 w-8 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+    role="status">
+    <span
+      class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+      >Loading...</span
+    >
+  </div>
+</template>
